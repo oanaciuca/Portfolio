d4d9055e19eeeda788b10116e3673a6b91b1182d
oanaciuca <oana.daniela.ciuca@gmail>
2025-05-30 03:08:49 +0200

    ajout btn whatsapp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import logo        from '../assets/logo.gif';
 import githubIcon  from '../assets/github.svg';
 import mailIcon    from '../assets/mail.svg';
+import whatsappIcon from '../assets/whatsapp.svg';
 
 export default function Footer() {
   const year = new Date().getFullYear();
-
+  const phoneNumber = '40724173715';
+  const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}`;
+    
   return (
     <footer className="footer">
       <div className="footer__inner">
@@ -26,6 +29,18 @@ export default function Footer() {
           </a>
           <a href="#contact" aria-label="Aller à la section contact">
             <img src={mailIcon} alt="Contactez-moi" className="footer__icon" />
+          </a>
+          <a
+            href={whatsappLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Contactez-moi sur WhatsApp"
+          >
+            <img
+              src={whatsappIcon}
+              alt="WhatsApp"
+              className="footer__icon"
+            />
           </a>
         </div>
       </div>  
