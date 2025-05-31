800e83b066d9193434dcfa724e77c7841249751b
oanaciuca <oana.daniela.ciuca@gmail>
2025-05-31 05:02:35 +0200

    ajout hamborger pr menu header+ amélioration css responsif

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.gif';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 export default function Header() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <header className="header">
       <div className="header-container">
         <a href="/" className="header-logo">
           <img src={logo} alt="Logo OC" width="60" height="40"/>
         </a>
+
+        <button
+          className="hamburger"
+          onClick={toggleMenu}
+          aria-label="Menu mobile"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
         <nav>
-          <ul className="header-menu">
+          <ul className={`header-menu ${menuOpen ? 'open' : ''}`}>
             <li>
-              <a href="#about" className="header-menu-link">
+              <a href="#about" className="header-menu-link" onClick={closeMenu}>
                 A PROPOS
               </a>
             </li>
             <li>
-              <a href="#competences" className="header-menu-link">
+              <a href="#competences" className="header-menu-link" onClick={closeMenu}>
                 COMPÉTENCES
               </a>
             </li>
             <li>
-              <a href="#realisations" className="header-menu-link">
+              <a href="#realisations" className="header-menu-link" onClick={closeMenu}>
                 RÉALISATIONS
               </a>
             </li>
             <li>
-              <a href="#contact" className="header-menu-link">
+              <a href="#contact" className="header-menu-link" onClick={closeMenu}>
                 CONTACT
               </a>
             </li>
